e72a110ad39211f5a55837a2d885c38b07855c6e
KossiPascal <kossi.tsolegnagbo@aiesec.net>
2022-12-08 11:39:03 +0000

    Initial Commit

--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -25,7 +25,7 @@ function initSeleted(id) {
   });
 }
 
-function initDataTable(tableId,paging= true) {
+function initDataTable(tableId, paging = true) {
   $("#" + tableId)
     .DataTable({
       paging: paging,
@@ -76,6 +76,23 @@ function initDataTable(tableId,paging= true) {
     .appendTo("#" + tableId + "_wrapper .col-md-6:eq(0)");
 }
 
+function initJsGridTable(id, dataArrayOfJson, fields) {
+  $("#" + id).jsGrid({
+    height: "100%",
+    width: "100%",
+    sorting: true,
+    paging: false,
+    // filtering: true,
+    // editing: true,
+    // inserting: true,
+    // deleting: true,
+    autoload: true,
+
+    data: dataArrayOfJson,
+    fields: fields,
+  });
+}
+
 $(document).ready(function () {
   //   initDataTable('thinkmd_datatable');
   //   initDataTable('medic_datatable');
@@ -108,19 +125,135 @@ $(document).ready(function () {
   /* END PLAY SOUND FUNCTION */
 });
 
-function initJsGridTable(id, dataArrayOfJson, fields) {
-  $("#" + id).jsGrid({
-    height: "100%",
-    width: "100%",
-    sorting: true,
-    paging: false,
-    // filtering: true,
-    // editing: true,
-    // inserting: true,
-    // deleting: true,
-    autoload: true,
 
-    data: dataArrayOfJson,
-    fields: fields,
+function showToast(icon, title){
+  var Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
   });
+
+  Toast.fire({
+    icon: icon, //'success','info','error', 'warning','question',
+    title: title // text to show
+  })
+
 }
+
+
+$(document).ready(function () {
+  $('.toastrDefaultSuccess').click(function () {
+    toastr.success('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+  });
+  $('.toastrDefaultInfo').click(function () {
+    toastr.info('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+  });
+  $('.toastrDefaultError').click(function () {
+    toastr.error('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+  });
+  $('.toastrDefaultWarning').click(function () {
+    toastr.warning('Lorem ipsum dolor sit amet, consetetur sadipscing elitr.')
+  });
+
+  $('.toastsDefaultDefault').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultTopLeft').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      position: 'topLeft',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultBottomRight').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      position: 'bottomRight',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultBottomLeft').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      position: 'bottomLeft',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultAutohide').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      autohide: true,
+      delay: 750,
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultNotFixed').click(function () {
+    $(document).Toasts('create', {
+      title: 'Toast Title',
+      fixed: false,
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultFull').click(function () {
+    $(document).Toasts('create', {
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      icon: 'fas fa-envelope fa-lg',
+    })
+  });
+  $('.toastsDefaultFullImage').click(function () {
+    $(document).Toasts('create', {
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      image: '../../assets/img/user3-128x128.jpg',
+      imageAlt: 'User Picture',
+    })
+  });
+  $('.toastsDefaultSuccess').click(function () {
+    $(document).Toasts('create', {
+      class: 'bg-success',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultInfo').click(function () {
+    $(document).Toasts('create', {
+      class: 'bg-info',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultWarning').click(function () {
+    $(document).Toasts('create', {
+      class: 'bg-warning',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultDanger').click(function () {
+    $(document).Toasts('create', {
+      class: 'bg-danger',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+  $('.toastsDefaultMaroon').click(function () {
+    $(document).Toasts('create', {
+      class: 'bg-maroon',
+      title: 'Toast Title',
+      subtitle: 'Subtitle',
+      body: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+    })
+  });
+});
+
