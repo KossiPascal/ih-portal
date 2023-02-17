69bbfcdf08841a813f28d42b475778a99653b6ed
Kossi TSOLEGNAGBO <kossi.tsolegnagbo@aiesec.net>
2023-02-17 13:58:59 +0000

    Initial Commit

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -5,20 +5,13 @@ import { JsonDatabase } from "../json-data-source";
 import { Functions, isNotNull } from "../utils/functions";
 
 export class Middelware {
-  static authMiddleware = async (req: Request, res: Response, next:any) => {
+  static authMiddleware = async (req: Request, res: Response, next: any) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) return res.status(res.statusCode).send('Not authenticated!');
     const token = authHeader.split(' ')[1];
     const userId = req.body.userId;
-    const _repoUser = new JsonDatabase('users');
-    var users: User[] = (Object.values(_repoUser.getBy(userId)) as User[]);
-
-    if (isNotNull(users)) {
-      jwt.verify(token, Functions.Utils(users[0].roles).secretOrPrivateKey, function (err, decoded) {
-        return err ? res.status(res.statusCode).send(err) : next();
-      });
-    } else {
-      return res.status(res.statusCode).send('User Error!');
-    };
+    jwt.verify(token, Functions.Utils().secretOrPrivateKey, function (err, decoded) {
+      return err ? res.status(res.statusCode).send(err) : next();
+    });
   };
 }
